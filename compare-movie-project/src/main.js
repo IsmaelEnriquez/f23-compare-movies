4c8888bc2d87db58983f0a8dd5d571d82a67f82e
ismaelnrqz <ismaelnrqz@gmail.com>
2023-12-11 21:22:29 -0500

    lets see

--- compare-movie-project/src/main.js
+++ compare-movie-project/src/main.js
@@ -1,26 +1,17 @@
 import './style.css';
 import { getEl } from './mods/utils';
-import movieData from './movie-data.json'
-
+import movieData from './movie-data.json';
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  const formObj = Object.fromEntries(new FormData(e.target));
-  console.log(formObj);
-  addValue(formObj);
+  const form = e.target;
 
-    const form = e.target;
-    const movieTitleInput = form.movieTitle.value;
-    const criticScoreInput = form.criticScore.value;
-    const audienceScoreInput = form.audienceScore.value;
-    const domesticGrossInput = form.domesticGross.value;
-    const genreInput = form.genre.value;
+  const movieTitleInput = form.movieTitle.value.trim();
+  const matchedMovie = movieData.find((movie) => movie.title.toLowerCase() === movieTitleInput.toLowerCase());
 
-    document.querySelector('#movie-title-results').textContent = movieTitleInput;
-    document.querySelector('#critic-score-results').textContent = criticScoreInput;
-    document.querySelector('#audience-score-results').textContent = audienceScoreInput;
-    document.querySelector('#domestic-total-results').textContent = domesticGrossInput;
-    document.querySelector('#genre-results').textContent = genreInput;
+  if (matchedMovie) {
+    addValue(matchedMovie);
+  }
 };
 
 const setLocalStorageKey = (key, value) => {
@@ -28,8 +19,43 @@ const setLocalStorageKey = (key, value) => {
 };
 
 const addValue = (value) => {
-  const values = getLocalStorageKey('form-data') ?? [];
+  const values = getLocalStorageKey('form-data') || [];
   setLocalStorageKey('form-data', [...values, value]);
+
+  displayMovies();
+};
+
+const displayMovies = () => {
+  const moviesContainer = getEl('#movies');
+  const values = getLocalStorageKey('form-data') || [];
+
+  values.forEach((movie) => {
+    const movieDiv = document.createElement('div');
+    movieDiv.classList.add('movie-entry');
+
+    const titleHeading = document.createElement('h2');
+    titleHeading.textContent = movie.title;
+
+    const criticScoreParagraph = document.createElement('p');
+    criticScoreParagraph.textContent = `Critic Score: ${movie.criticScore}`;
+
+    const audienceScoreParagraph = document.createElement('p');
+    audienceScoreParagraph.textContent = `Audience Score: ${movie.audienceScore}`;
+
+    const domesticGrossParagraph = document.createElement('p');
+    domesticGrossParagraph.textContent = `Domestic Gross: ${movie.domesticTotal}`;
+
+    const genreParagraph = document.createElement('p');
+    genreParagraph.textContent = `Genre: ${movie.genre}`;
+
+    movieDiv.appendChild(titleHeading);
+    movieDiv.appendChild(criticScoreParagraph);
+    movieDiv.appendChild(audienceScoreParagraph);
+    movieDiv.appendChild(domesticGrossParagraph);
+    movieDiv.appendChild(genreParagraph);
+
+    moviesContainer.appendChild(movieDiv);
+  });
 };
 
 const getLocalStorageKey = (key) => {
@@ -46,6 +72,9 @@ export const setValue = (key, names) => setLocalStorageKey(key, names);
 
 const resetLocalStorage = () => {
   localStorage.clear();
+
+  const moviesContainer = getEl('#movies');
+  moviesContainer.textContent = '';
 };
 
 const main = () => {
@@ -53,6 +82,9 @@ const main = () => {
   document.getElementById('reset').addEventListener('click', resetLocalStorage);
   console.log(getValue('form-data'));
   console.log(movieData);
+
+  displayMovies();
 };
 
 main();
+
